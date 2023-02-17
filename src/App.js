6fcdba57a0256bf53b18a437f34f329440cfbb30
uonod1323 <uonod1323@naver.com>
2023-02-17 17:26:50 +0900

    리액트트트트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  let post = '강남 우동 맛집'; //자료 잠깐 저장할 땐 변수
+  let [글제목, b] = useState(['여자 코트 추천', '디자이어 그랑프리 랭킹', '까짓거 함 해보죠']);
+  let [따봉, 따봉변경] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>{글제목[0]}<span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
