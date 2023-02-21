ae9c812de5299c308242776d174722a397729212
uonod1323 <uonod1323@naver.com>
2023-02-21 17:25:05 +0900

    뭐햇더라...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {useState} from 'react'
 function App() {
 
   let post = '강남 우동 맛집'; //자료 잠깐 저장할 땐 변수
-  let [글제목, b] = useState(['여자 코트 추천', '디자이어 그랑프리 랭킹', '까짓거 함 해보죠']);
+  let [글제목, 제목변경] = useState(['남자 코트 추천', '디자이어 그랑프리 랭킹', '까짓거 함 해보죠']);
   let [따봉, 따봉변경] = useState(0);
 
   return (
@@ -24,6 +24,17 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      <button onClick={()=>{   
+        let copy = [...글제목];
+        copy.sort();
+        console.log(copy);
+        제목변경(copy);     
+        }}>가나다순정렬</button>
+      <button onClick={()=>{   
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        제목변경(copy);
+        }}>바꿔줘</button>
     </div>
   );
 }
