9a969c6e312cd589ea7ae6b3615536e06df7adea
uonod1323 <uonod1323@naver.com>
2023-02-21 22:41:32 +0900

    모달창만들기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   let post = '강남 우동 맛집'; //자료 잠깐 저장할 땐 변수
   let [글제목, 제목변경] = useState(['남자 코트 추천', '디자이어 그랑프리 랭킹', '까짓거 함 해보죠']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -13,7 +14,9 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{글제목[0]}<span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
+        <h4 onClick={()=>{
+          setModal(!modal);   
+        }}>{글제목[0]}<span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
@@ -35,8 +38,23 @@ function App() {
         copy[0] = '여자 코트 추천';
         제목변경(copy);
         }}>바꿔줘</button>
+        
+        {
+          modal == true ? <Modal/> : null
+        }
+
     </div>
   );
+}
+
+function Modal(){
+  return(
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
 }
 
 export default App;
